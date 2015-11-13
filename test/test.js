1148bd65f6b3640330d74fcf074143fff9d8e777
Hedy Wang <hedywings@gmail.com>
2015-11-13 02:23:34 -0800

    finish security modle

--- test/test.js
+++ test/test.js
@@ -1,3 +1,21 @@
-var blemgr = require('../lib/bleMgr');
+var blemgr = require('../lib/bleMgr'),
+	spConfig = {
+        path: '/dev/ttyUSB0',
+        baudRate: 115200,
+        rtscts: true,
+        flowControl: true
+    };
 
-blemgr.start(function() {});
+blemgr.start(spConfig, bleApp);
+
+function bleApp () {
+	// blemgr.devmgr.bleDevices[1].servs['0xfff0'].chars['0xfff4'].readDesc();
+	// setTimeout(function () {
+	// 	console.log(blemgr.devmgr.bleDevices[1].sm);
+	// }, 3000);
+
+	// setTimeout(function () {
+	// 	console.log(blemgr.devmgr.bleDevices[2]);
+	// }, 15000);
+	
+}
