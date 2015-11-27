88c195e53fc50bce324b0d15876096a3756500fc
Hedy Wang <hedywings@gmail.com>
2015-11-27 00:40:25 -0800

    refactoring

--- test/test.js
+++ test/test.js
@@ -1,7 +1,7 @@
 var _ = require('lodash');
 
-var blemgr = require('../lib/bleMgr'),
-    servConstr = require('../lib/service/bleServConstr');
+var bShepherd = require('../lib/ble-shepherd'),
+    exampleServ = require('../lib/service/example'),
     spConfig = {
         path: '/dev/ttyUSB0',
         options: {
@@ -11,48 +11,44 @@ var blemgr = require('../lib/bleMgr'),
         }
     };
 
-var charsInfo = [
-    {uuid: '0xFFF1', permit: 1, prop: 2, desc: 'CHAR1', name: 'CHAR1', val: 20},
-    {uuid: '0xFFF2', permit: 1, prop: 2, desc: 'CHAR2', name: 'CHAR2', val: 40},
-	{uuid: '0xFFF3', permit: 3, prop: 8, desc: 'CHAR3', name: 'CHAR3', val: 60},
-	{uuid: '0xFFF4', permit: 1, prop: 2, name: 'CHAR4', val: 80},
-	{uuid: '0xFFF5', permit: 1, prop: 2, name: 'CHAR5', val: 100},
-    {uuid: '0x2902', permit: 1, prop: 2, name: 'CHAR6', val: 1},
-    {uuid: '0x2902', permit: 1, prop: 2, name: 'CHAR7', val: 2},
-    {uuid: '0x2902', permit: 1, prop: 2, name: 'CHAR8', val: 2},
-    {uuid: '0x2902', permit: 1, prop: 2, name: 'CHAR9', val: 2},
-];
-
-var service = new servConstr('centralServ', '0xFFF0', charsInfo);
-
-blemgr.preExec = preExec;
-blemgr.start(spConfig, bleApp);
+bShepherd.preExec = preExec;
+bShepherd.start(spConfig, bleApp);
 
 function preExec () {
-    blemgr.regGattDefs('service', [{name: 'Test', uuid: '0xFFF0'}, {name: 'SimpleKeys', uuid: '0xffe0'}, {name: 'Accelerometer', uuid: '0xffa0'}]);
-    blemgr.regGattDefs('characteristic', [{name: 'KeyPressState', uuid: '0xffe1', params: ['Enable'], types: ['uint8']}, {name: 'Enable', uuid: '0xffa1'}, {name: 'AccelerometerX', uuid: '0xffa3'},
-        {name: 'AccelerometerY', uuid: '0xffa4'}, {name: 'AccelerometerZ', uuid: '0xffa5'}]);
+    bShepherd.regGattDefs('service', [
+        {name: 'Test', uuid: '0xFFF0'}, 
+        {name: 'SimpleKeys', uuid: '0xffe0'}, 
+        {name: 'Accelerometer', uuid: '0xffa0'}]);
+
+    bShepherd.regGattDefs('characteristic', [
+        {name: 'KeyPressState', uuid: '0xffe1', params: ['Enable'], types: ['uint8']}, 
+        {name: 'Enable', uuid: '0xffa1'}, 
+        {name: 'AccelerometerX', uuid: '0xffa3'},
+        {name: 'AccelerometerY', uuid: '0xffa4'}, 
+        {name: 'AccelerometerZ', uuid: '0xffa5'}, 
+        {name: 'FFF3', uuid: '0xfff3', params: ['val'], types: ['uint8']},
+        {name: 'FFF1', uuid: '0xfff1', params: ['val'], types: ['uint8']}]);
 }
 
 function bleApp () {
-    var keyFob = blemgr.devmgr.findDev('0x544a165e1f53');
+    // var keyFob = bShepherd.devmgr.findDev('0x544a165e1f53');
 
-    blemgr.addLocalServ(service);
-    keyFob.servs['0xffe0'].chars['0xffe1'].processInd = processKeyFobInd;
-    keyFob.servs['0xffe0'].chars['0xffe1'].setConfig(true).then(function (result) {
-        console.log(result);
-    }).fail(function (err) {
-        console.log(err);
-    });
+    bShepherd.addLocalServ(exampleServ);
+    // keyFob.servs['0xffe0'].chars['0xffe1'].processInd = processKeyFobInd;
+    // keyFob.servs['0xffe0'].chars['0xffe1'].setConfig(true).then(function (result) {
+    //     console.log(result);
+    // }).fail(function (err) {
+    //     console.log(err);
+    // });
 
     // setTimeout(function () {
-    //     var keyFob = blemgr.devmgr.findDev('0x544a165e1f53');
+    //     var keyFob = bShepherd.devmgr.findDev('0x544a165e1f53');
     //     _.forEach(keyFob.servs, function (serv) {
     //         console.log(serv);
     //     });
     // }, 30000);
 
-	blemgr.on('ind', function(msg) {
+	bShepherd.on('IND', function(msg) {
         switch (msg.type) {
             case 'DEV_INCOMING':
                 break;
@@ -61,6 +57,8 @@ function bleApp () {
             case 'ATT_IND':
                 break;
             case 'PASSKEY_NEED':
+                break;
+            case 'LOCAL_SERV_ERR':
                 break;
         }
     });
